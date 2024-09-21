9b43350317b0296b5ad747717105527b1fe15d76
johnnyFR26 <149261007+johnnyFR26@users.noreply.github.com>
2024-09-21 18:26:06 -0300

    create main routes

--- animaldle/src/app/app.routes.ts
+++ animaldle/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { GameComponent } from './game/game.component';
+import { RankingComponent } from './ranking/ranking.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'home', component: HomeComponent},
+    {path: 'game', component: GameComponent},
+    {path: 'ranking', component: RankingComponent},
+    {path: 'logIn', component: LogInComponent},
+    {path: 'signIn', component: SignInComponent}
+];
