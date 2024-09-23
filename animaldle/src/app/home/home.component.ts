034219e0541896372ea825207fcc920a2d80a4a8
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2024-09-23 09:14:34 -0300

    Fixes images

--- animaldle/src/app/home/home.component.ts
+++ animaldle/src/app/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { GameComponent } from '../game/game.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, GameComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
