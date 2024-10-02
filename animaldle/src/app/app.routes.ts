6f9f0fccdf9abee50719a6c7a104bf49c9477053
johnnyFR26 <johnny.rabelo.cf@gmail.com>
2024-10-02 09:39:14 -0300

    add animation leafs

--- animaldle/src/app/app.routes.ts
+++ animaldle/src/app/app.routes.ts
@@ -1,4 +1,4 @@
-import { Routes } from '@angular/router';
+import { Routes, CanActivateFn } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { GameComponent } from './game/game.component';
 import { RankingComponent } from './ranking/ranking.component';
@@ -7,7 +7,7 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
-    {path: '**', redirectTo: 'home', pathMatch: 'full'},
+    // {path: '**', redirectTo: 'home', pathMatch: 'full'},
     {path: 'home', component: HomeComponent},
     {path: 'game', component: GameComponent, canActivate: [authGuard]},
     {path: 'ranking', component: RankingComponent},
--- animaldle/src/app/home/home.component.ts
+++ animaldle/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { GameComponent } from '../game/game.component';
 
@@ -9,10 +9,75 @@ import { GameComponent } from '../game/game.component';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
-
+export class HomeComponent implements AfterViewInit {
   StartGame() {
     console.log('Starting...')
   }
+
+  private ctx!: CanvasRenderingContext2D;
+  private width: number = window.innerWidth;
+  private height: number = window.innerHeight;
+  private items: Item[] = [];
+
+  private rand(m: number, M: number): number {
+    return Math.random() * (M - m) + m;
+  }
+
+  ngAfterViewInit(): void {
+    const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
+    this.ctx = canvas.getContext('2d')!;
+    canvas.width = this.width;
+    canvas.height = this.height;
+
+    for (let i = 0; i < 20; i++) this.items.push(new Item(this.rand, this.width, this.height));
+
+    this.loop();
+  }
+
+  private loop(): void {
+    this.ctx.clearRect(0, 0, this.width, this.height);
+    this.items.forEach(item => item.move(this.ctx));
+    requestAnimationFrame(() => this.loop());
+  }
+}
+
+class Item {
+  private x: number = 0;
+  private y: number = 0;
+  private angle: number = 0;
+  private speed: number = 0;
+  private IMG: HTMLImageElement;
+  private readonly h: number = 32;
+  private readonly w: number = 32;
+
+  constructor(private rand: (m: number, M: number) => number, width: number, height: number) {
+    this.IMG = new Image();
+    this.IMG.src = '../../assets/public/foia.png';
+    this.start(width, height);
+  }
+
+  private start(width: number, height: number): void {
+    this.x = this.rand(0, width - this.w / 2);
+    this.y = this.rand(0, height);
+    this.angle = this.rand(0, Math.PI * 2);
+    this.speed = this.rand(0.01, 0.1);
+  }
+
+  public move(ctx: CanvasRenderingContext2D): void {
+    if (this.y < -this.h - this.IMG.height) {
+      this.start(ctx.canvas.width, ctx.canvas.height);
+      this.y = ctx.canvas.height + this.IMG.height;
+    }
+    this.y -= this.speed / 0.1;
+    this.angle = Math.cos(this.y / 20);
+    console.log(this.angle);
+
+    ctx.save();
+    ctx.translate(this.x, this.y);
+    ctx.rotate(this.angle);
+    ctx.drawImage(this.IMG, -this.w / 2, -this.h / 2);
+    ctx.restore();
+  }
+
 
 }
