0ce04f5029ac7953a78820402d3e8f21cc60ce1b
Alyson Gabriel Matos dos Santos <alysongabriel07@gmail.com>
2024-10-15 19:08:48 -0300

    Change generation of Leafs, changed the Login and Signin screens

--- animaldle/src/app/home/home.component.ts
+++ animaldle/src/app/home/home.component.ts
@@ -29,8 +29,8 @@ export class HomeComponent implements AfterViewInit {
     const canvas = document.getElementById('myCanvas') as HTMLCanvasElement;
     this.ctx = canvas.getContext('2d')!;
     const TempItem = new Item(this.rand, this.width, this.height,0);
-    this.colunas = Math.floor(this.width*1.5/Item.w);
-    this.linhas = Math.floor(this.height/(Item.h*this.colunas))*2;
+    this.colunas = Math.floor(this.width*1.8/(Item.w*2));
+    this.linhas = Math.floor(this.height*1.5/(Item.h*2));
     canvas.width = this.width;
     canvas.height = this.height;
 
@@ -50,19 +50,20 @@ class Item {
   private x: number = 0;
   private y: number = 0;
   private angle: number = 0;
+  private anglespeed: number = 0;
   private speed: number = 0;
   private IMG: HTMLImageElement;
   private static currentImage: number = 0;
   private static previousWidth: number = 1;
-  private static previousHeight: number =1;
+  private static previousHeight: number = 1;
   public static h: number = 0;
   public static w: number = 0;
 
   constructor(private rand: (m: number, M: number) => number, width: number, height: number, colunas: number) {
     this.IMG = new Image();
-    this.IMG.src = '../../assets/public/foia.png';
+    this.IMG.src = '../../assets/public/foia'+Math.round(rand(0.5,5.5))+'.png';
     Item.w = 3000*(width/1536)*0.07;
-    Item.h = 3000*(width/1536)*0.07/4;
+    Item.h = 3000*(width/1536)*0.07/4/1.5;
     this.start(width, height, colunas);
   }
 
@@ -70,27 +71,29 @@ class Item {
     Item.currentImage++;
     if((Item.currentImage)%colunas==0){
       this.x = Item.w;
-      this.y = Item.previousHeight - Item.h;
+      this.y = Item.previousHeight + Item.h*2;
     }
     else{
-      this.x = Item.previousWidth+Item.w;
-      this.y = Item.previousHeight+Item.h;
+      this.x = Item.previousWidth+Item.w*2;
+      this.y = Item.previousHeight;
     }
     Item.previousHeight = this.y;
     Item.previousWidth = this.x;
     this.angle = this.rand(0, Math.PI * 2);
-    this.speed = 0.1;
+    this.speed = this.rand(0.05, 0.15);
+    console.log("x= "+this.x+"\ny= "+this.y)
   }
 
   public move(ctx: CanvasRenderingContext2D): void {
-    if (this.y < -Item.h*2) {
-      this.y = ctx.canvas.height + Item.h*2;
-    }if (this.x < 0) {
+    if (this.y < -Item.h*8) {
+      this.y = ctx.canvas.height + Item.h*8;
+    }if (this.x < -Item.w*2) {
       this.x = ctx.canvas.width + Item.w*2;
     }
     this.y -= this.speed / 0.1;
     this.x -= this.speed / 0.1;
-    this.angle = Math.cos(this.y / 20);
+    this.anglespeed -= 1;
+    this.angle += Math.cos(this.anglespeed / 50)/250;
     ctx.save();
     ctx.translate(this.x, this.y);
     ctx.rotate(this.angle);
@@ -98,6 +101,5 @@ class Item {
     ctx.drawImage(this.IMG, -Item.w / 2, -Item.h / 2, this.IMG.height*(ctx.canvas.width/1536)*0.07, this.IMG.width*(ctx.canvas.width/1536)*0.07);
     ctx.restore();
   }
-
 
 }
